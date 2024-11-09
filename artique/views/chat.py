45847d3354d6461caed803c7fc07bd736e5ff482
Park Sanghyeok <huanhuan1235@naver.com>
2024-11-10 02:59:27 +0900

    summary 5개 구현

--- artique/views/chat.py
+++ artique/views/chat.py
@@ -1,7 +1,7 @@
 import openai
 from flask import Blueprint, jsonify, request
 from artique import db
-from artique.models import Chat
+from artique.models import Chat, Sentence
 import os
 from dotenv import load_dotenv
 
@@ -51,3 +51,43 @@ def save_chat():
     except Exception as e:
         db.session.rollback()
         return jsonify({'error': str(e)}), 400
+
+# Chat 데이터 추출해서 요약한 뒤, 5개 Sentence를 저장 및 반환하는 API 엔드포인트
+@bp.route('/summary', methods=['POST'])
+def summarize_chat():
+    data = request.get_json()
+    receiver_id = data['receiver']
+
+    try:
+        # 채팅 데이터 추출
+        chats = Sentence.query.join(Chat).filter(Chat.receiver == receiver_id).all()
+        print(chats)
+        chat_text = ' '.join([chat.chat.message for chat in chats])
+        chat_text += '이 내용을 바탕으로 감성적인 문장들과 명언을 5개 만들어줘. 5개의 문장을 list로 반환해줘.'
+
+        # AI API 호출
+        openai.api_key = os.getenv('OPENAI_API_KEY')
+        response = openai.chat.completions.create(
+                model="gpt-4o-mini-2024-07-18",  # 사용할 모델 이름 (예: gpt-3.5-turbo 등)
+                messages=[{"role": "user", "content": chat_text}],
+                max_tokens=300
+            )
+
+        # AI 응답을 DB에 저장
+        gpt_response = response.choices[0].message.content.strip()
+
+        # AI 응답을 Sentence로 분리해서 DB에 저장
+        sentences = gpt_response.split('\n')
+        for sentence in sentences[2:-2]:
+            new_sentence = Sentence(
+                receiver_id=receiver_id,
+                summary=sentence
+            )
+            db.session.add(new_sentence)
+        db.session.commit()
+
+        return jsonify({'sentences': sentences[2:-2]}), 201
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({'error': str(e)}), 400
+        
