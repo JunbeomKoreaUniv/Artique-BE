a0987634ce84c9d5723a88251a5f494bb446c427
JunbeomKoreaUniv <ku2soon@korea.ac.kr>
2024-11-09 16:52:52 +0900

    feat:: Picture, Chat 모델 추가

--- artique/models.py
+++ artique/models.py
@@ -1,7 +1,43 @@
 from artique import db
 
 class User(db.Model):
+    #primary key
     id = db.Column(db.Integer, primary_key=True)
+
     email = db.Column(db.String(200), unique= True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
     nickname = db.Column(db.String(200), unique=True, nullable=False)
+
+    # User 테이블에서 user.picture_set << 이런 식으로 그림 테이블 접근 가능.
+
+
+class Picture(db.Model):
+    #primary key
+    id = db.Column(db.Integer, primary_key=True)
+
+    # foreign key
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'))
+    user = db.relationship('User', backref=db.backref('picture_set'))
+
+    name = db.Column(db.String(200))
+    artist = db.Column(db.String(200))
+    gallery = db.Column(db.String(200))
+    start_date = db.Column(db.DateTime)
+    end_date = db.Column(db.DateTime)
+    custom_prompt = db.Column(db.Text)
+    custom_explanation = db.Column(db.Text, nullable=False)
+    custom_question = db.Column(db.Text)
+    sound = db.Column(db.Text)
+
+
+class Chat(db.Model):
+    #primary key
+    id = db.Column(db.Integer, primary_key=True)
+
+    #foreign key
+    picture_id = db.Column(db.Integer, db.ForeignKey('picture.id'))
+    picture = db.relationship('Picture', backref=db.backref('chat_set'))
+
+    message = db.Column(db.Text)
+    sender = db.Column(db.String(200))
+    receiver = db.Column(db.String(200))
