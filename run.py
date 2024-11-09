c13e48b89d761d39780cfee498a30f83b4ba5ba3
Park Sanghyeok <huanhuan1235@naver.com>
2024-11-10 05:55:46 +0900

    try 0.0.0.0

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+import os
 from artique import create_app, db
 
 app = create_app()
@@ -7,4 +8,5 @@ with app.app_context():
     db.create_all()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Render의 PORT 환경 변수를 사용
+    app.run(host="0.0.0.0", port=port, debug=True)  # 모든 인터페이스에서 접근 가능하게 설정
